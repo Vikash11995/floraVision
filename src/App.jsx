4f9f8943e71d460ae6db068616b4128f7e55911c
Vikash Yadav <154899326+Vikash11995@users.noreply.github.com>
2026-07-02 13:40:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero'
 const App = () => {
   return (
 <div className="">
-      <header className=' w-full h-screen md:h-[300vh] bg-[url("./src/assets/fa0765984e44f85e1a04562f820f1492949a1257.jpg")] bg-cover bg-center'>
+      <header className=' w-full h-screen md:h-[300vh] bg-[url("assets/fa0765984e44f85e1a04562f820f1492949a1257.jpg")] bg-cover bg-center'>
       <nav>
         <Navbar/>
       </nav>
